8fbf2a713682df64fa81c2a91305d9e052a674da
TabindaAnmol <123598574+TabindaAnmol@users.noreply.github.com>
2024-04-03 13:18:30 +0500

    updated

--- Schemas/UserSchema.js
+++
@@ -1,8 +0,0 @@
-const references=require("../References/customReferences")
-const adminSchema=references.mongoose.Schema({
-    "name":String,
-    "password":String,
-}, {
-    timestamps:{createdAt:'created_at',updatedAt:'updated_at'},
-})
-module.exports=adminSchema;
--- Schemas/UserSchemas/PlumberSchema.js
+++ Schemas/UserSchemas/PlumberSchema.js
@@ -1,1 +1,8 @@
-console.log('jjjjjjjjjjjjjj')
+const references=require("../../References/customReferences")
+const plumberSchema=references.mongoose.Schema({
+    "name":String,
+    "password":String,
+}, {
+    timestamps:{createdAt:'created_at',updatedAt:'updated_at'},
+})
+module.exports=plumberSchema;
