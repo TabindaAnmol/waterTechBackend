b411359ad15cb7450368443f6ff79a2df2597a10
Anam-Anmol <123668253+Anam-Anmol@users.noreply.github.com>
2024-04-14 14:05:53 +0500

    updated

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const plumberAuthRoutes = require("./Routes/AuthRoutes/PlumberAuthRoutes");
 const propertyOwnerRoutes=require('./Routes/UsersRoutes/PropertyOwnerRoutes')
 const plumberRoutes=require('./Routes/UsersRoutes/PlumberRoutes')
 
-express.use(references.express.static(root + "/Public/Assets/Images/Profiles"));
-express.use("/PropertyOwners", references.express.static("PropertyOwners"));
-express.use("/Plumbers", references.express.static("Plumbers"));
+express.use(references.express.static(root + "/Public/Assets/Images"));
+express.use("/Profiles/PropertyOwners", references.express.static("PropertyOwners"));
+express.use("/Profiles/Plumbers", references.express.static("Plumbers"));
 
 express.use('/propertyOwner/auth',propertyOwnerAuthRoutes);
 express.use('/plumber/auth',plumberAuthRoutes);
