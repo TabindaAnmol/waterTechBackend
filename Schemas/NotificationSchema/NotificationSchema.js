99d0a6e805fdb14e0ebd5e361a3a7a01519c0b31
Anam-Anmol <123668253+Anam-Anmol@users.noreply.github.com>
2024-04-23 15:46:23 +0500

    updated

--- Schemas/NotificationSchema/NotificationSchema.js
+++ Schemas/NotificationSchema/NotificationSchema.js
@@ -1,18 +1,22 @@
 const references = require("../../References/customReferences.js");
-const notificationSchema = references.mongoose.Schema({
-  "title": String,
-  "body": String,
-  plumberId: {
-    type: references.mongoose.Schema.Types.ObjectId,
-    ref: "plumbers",
+const notificationSchema = references.mongoose.Schema(
+  {
+    title: String,
+    message: String,
+    plumberId: {
+      type: references.mongoose.Schema.Types.ObjectId,
+      ref: "plumbers",
+      required: false,
+    },
+    propertyOwnerId: {
+      type: references.mongoose.Schema.Types.ObjectId,
+      ref: "propertyOwners",
+      required: false,
+    },
   },
-  propertyOwnerId: {
-    type: references.mongoose.Schema.Types.ObjectId,
-    ref: "propertyOwners",
-  },
-  
-}, {
-  // timestamps: true ,
-  timestamps:{createdAt:'created_at',updatedAt:'updated_at'},
-});
+  {
+    // timestamps: true ,
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+  }
+);
 module.exports = notificationSchema;
