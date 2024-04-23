74390279d1f69706eeb4dfc667772c9fefe69cf5
Anam-Anmol <123668253+Anam-Anmol@users.noreply.github.com>
2024-04-23 14:28:18 +0500

    backend

--- Routes/JobRoutes/JobRoutes.js
+++ Routes/JobRoutes/JobRoutes.js
@@ -6,8 +6,8 @@ app.use(references.cors());
 
 app.post("/postJob", formdata, async (req, res) => {
   console.log(req.body);
-  const { lineId,plumberId,date,time,jobInstructions } = { ...req.body };
-  console.log(lineId,plumberId,date,time)
+  const { lineId, plumberId, date, time, jobInstructions } = { ...req.body };
+  console.log(lineId, plumberId, date, time);
   const newJob = await jobController.createJob(req.body);
   console.log(newJob);
   if (newJob) {
@@ -16,29 +16,64 @@ app.post("/postJob", formdata, async (req, res) => {
     res.send({ added: false });
   }
 });
+app.post("/jobDetail", formdata, async (req, res) => {
+  console.log(req.body);
+  const { jobId } = req.body;
+  const jobDetail = await jobController.viewJobDetail(jobId);
+  console.log("..................................");
+  console.log(jobDetail);
+  console.log("..................................");
+
+  if (jobDetail) {
+    res.send({ match: true, jobDetail: jobDetail });
+  } else {
+    res.send({ match: false });
+  }
+});
 app.post("/singlePropertyOwnerJobsWithStatus", formdata, async (req, res) => {
   console.log(req.body);
-  const { propertyOwnerId ,jobStatus} = req.body;
-  const jobs = await jobController.viewSinglePropertyOwnerJobsWithStatus(propertyOwnerId,jobStatus);
+  const { propertyOwnerId, jobStatus } = req.body;
+  const jobs = await jobController.viewSinglePropertyOwnerJobsWithStatus(
+    propertyOwnerId,
+    jobStatus
+  );
   if (jobs.length > 0) {
     res.send({ match: true, jobs: jobs });
   } else {
     res.send({ match: false, jobs: [] });
   }
 });
-app.post("/jobDetail", formdata, async (req, res) => {
+app.post("/updateJobStatus", formdata, async (req, res) => {
   console.log(req.body);
-  const { jobId } = req.body;
-  const jobDetail = await jobController.viewJobDetail(jobId);
-  console.log('..................................')
-  console.log(jobDetail)
-  console.log('..................................')
-
-  if (jobDetail) {
-    res.send({ match: true, jobDetail: jobDetail });
+  const { jobId, jobStatus } = req.body;
+  const updated = await jobController.updateJobStatus(jobId, jobStatus);
+  if (updated == 1) {
+    res.send({ updated: true });
   } else {
-    res.send({ match: false});
+    res.send({ updated: false });
   }
+});
+app.post("/propertyOwnerJobStats", formdata, async (req, res) => {
+
+  console.log('/////////////////////propertyOwnerJobStats');
+  console.log(req.body);
+  const { propertyOwnerId } = req.body;
+  console.log(propertyOwnerId)
+  const stats = await jobController.propertyOwnerJobStats(
+   propertyOwnerId
+  );
+  res.send(stats);
+});
+app.post("/plumberJobStats", formdata, async (req, res) => {
+
+  console.log('/////////////////////plumberJobStats');
+  console.log(req.body);
+  const { plumberId } = req.body;
+  console.log(plumberId)
+  const stats = await jobController.propertyOwnerJobStats(
+    plumberId
+  );
+  res.send(stats);
 });
 
 module.exports = app;
